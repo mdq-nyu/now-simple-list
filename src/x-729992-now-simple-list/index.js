8f6e11872f819cac6e229e432a0a48fbd5332a37
mdq-nyu <mdq204@nyu.edu>
2023-11-07 15:47:38 -0500

    Setup dependencies and test development environment

--- src/x-729992-now-simple-list/index.js
+++ src/x-729992-now-simple-list/index.js
@@ -1,15 +1,17 @@
-import {createCustomElement} from '@servicenow/ui-core';
-import snabbdom from '@servicenow/ui-renderer-snabbdom';
-import styles from './styles.scss';
+import { createCustomElement } from "@servicenow/ui-core";
+import snabbdom from "@servicenow/ui-renderer-snabbdom";
+import styles from "./styles.scss";
 
-const view = (state, {updateState}) => {
+const view = (state, { updateState }) => {
 	return (
-		<div></div>
+		<div>
+			<h1>Hello World!</h1>
+		</div>
 	);
 };
 
-createCustomElement('x-729992-now-simple-list', {
-	renderer: {type: snabbdom},
+createCustomElement("x-729992-now-simple-list", {
+	renderer: { type: snabbdom },
 	view,
-	styles
+	styles,
 });
