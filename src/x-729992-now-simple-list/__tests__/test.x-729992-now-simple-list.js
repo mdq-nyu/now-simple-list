e1a28c2d6bfd9ceeb171fb0338e691bdb107425b
mdq-nyu <mdq204@nyu.edu>
2023-11-14 12:22:32 -0500

    initial prettier formatting

--- src/x-729992-now-simple-list/__tests__/test.x-729992-now-simple-list.js
+++ src/x-729992-now-simple-list/__tests__/test.x-729992-now-simple-list.js
@@ -1,5 +1,5 @@
-describe('x-729992-now-simple-list Test', () => {
-	it('should be true', () => {
+describe("x-729992-now-simple-list Test", () => {
+	it("should be true", () => {
 		expect(true).toBe(true);
 	});
 });
