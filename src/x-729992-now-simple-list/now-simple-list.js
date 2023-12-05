37b4355c4ad04ae660b9506baddfcebb816a42b5
mdq-nyu <mdq204@nyu.edu>
2023-12-05 12:56:55 -0500

    Updated properties scheme to conform to SN standards

--- src/x-729992-now-simple-list/now-simple-list.js
+++ src/x-729992-now-simple-list/now-simple-list.js
@@ -2,6 +2,7 @@ import { createCustomElement } from "@servicenow/ui-core";
 import snabbdom from "@servicenow/ui-renderer-snabbdom";
 import styles from "./styles.scss";
 import view from "./view.js";
+import actions from "./actions.js";
 
 createCustomElement("x-729992-now-simple-list", {
 	renderer: { type: snabbdom },
@@ -9,14 +10,17 @@ createCustomElement("x-729992-now-simple-list", {
 	styles,
 	properties: {
 		table: {
-			default: "tasl",
+			default: "task",
 		},
 		fields: {
 			default: ["number", "assigned_to", "sys_created_on"],
 			schema: {
 				type: "array",
-				items: "string",
+				items: {
+					type: "string",
+				},
 			},
 		},
 	},
+	actionHandlers: actions,
 });
